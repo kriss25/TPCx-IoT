af8f46f5463b1d63ea15513120c06b6618499f2f
kriss25 <72523695+kriss25@users.noreply.github.com>
2020-10-09 16:20:57 +0200

    Update TPC-IoT-client.sh

--- TPC-IoT-client.sh
+++ TPC-IoT-client.sh
@@ -20,7 +20,6 @@ LOGFILE_NAME=${11}
 mkdir -p ./logs
 start=`date +%s%3N`
 
-
 echo "$PWD $prefix $clientId $LOGFILE_NAME $i" | tee -a tmp.log
 
 echo -e "${green}$sep${NC}" | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
@@ -32,20 +31,13 @@ echo "check 1" | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_
 echo -e "${green}Starting IoT Run $i output being return to $PWD/logs/IoT-run-time-$LOGFILE_NAME$i.txt ${NC}" | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
 echo "check 2" | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
 
-# Command for running workload IoT based on the YCSB params
-
 start_string=`grep insertstart $PWD/tpcx-iot/workloads/$WORKLOAD`
 INSERT_START=$(echo $start_string | cut -d'=' -f2)
 operation_count_string=`grep operationcount $PWD/tpcx-iot/workloads/$WORKLOAD`
 DATABASE_RECORDS_COUNT=$(echo $operation_count_string | cut -d'=' -f2)
-# Invoke the instance.sh here. 
 
-# echo ">>>>>>>> $PWD/TPC-IoT-instances.sh $DATABASE_RECORDS_COUNT $NUM_INSTANCES $NUM_THREADS $INSERT_START $clientId $DATABASE_CLIENT $LOGFILE_NAME"
 echo ">>>>>>>> $PWD/TPC-IoT-instances.sh $DATABASE_RECORDS_COUNT $NUM_INSTANCES $NUM_THREADS $INSERT_START $clientId $DATABASE_CLIENT $LOGFILE_NAME"  | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
 $PWD/TPC-IoT-instances.sh $DATABASE_RECORDS_COUNT $NUM_INSTANCES $NUM_THREADS $INSERT_START $clientId $DATABASE_CLIENT $PWD $SUT_PARAMETERS $LOGFILE_NAME
-
-#  Command for running workload IoT based on the YCSB params
-#(time  $PWD/tpcx-iot/bin/tpcx-iot run $DATABASE_CLIENT -P $PWD/tpcx-iot/workloads/$WORKLOAD -p columnfamily=cf -p recordcount=$DATABASE_RECORDS_COUNT -p client=$clientId -threads $NUM_THREADS) 2> >(tee $PWD/logs/IoT-Workload-run-time-$LOGFILE_NAME$i.txt)
 
 result=$?
 
@@ -57,9 +49,6 @@ else
 fi
 cat $PWD/logs/IoT-Workload-run-time-$LOGFILE_NAME$i-$clientId.txt >> $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
 
-# Capture Results
- # Capture Success/Failure
- # TODO :: Code for capturing results success and failure and log them back to the workload log file
 echo "" | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
 echo "" | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
 echo -e "${green}======== Workload IoT Run Result SUCCESS ========${NC}" | tee -a $PWD/logs/TPCx-IoT-result-"$prefix"-"$clientId-$LOGFILE_NAME$i".log
